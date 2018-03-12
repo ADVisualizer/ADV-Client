cd1c953056acaaaf480605a5d74bd7bda5bfeaf5
Michael Wieland <michi.wieland@hotmail.com>
2018-03-12 16:23:00 +0100

    use static launch method

--- src/main/java/ch/hsr/Test.java
+++ src/main/java/ch/hsr/Test.java
@@ -1,14 +1,15 @@
 package ch.hsr;
 
-
-import ch.adv.lib.AdvCore;
+import ch.adv.lib.ADV;
 
 import java.io.IOException;
 
 public class Test {
 
     public static final void main(String[] args) throws IOException {
-        AdvCore core = new AdvCore();
-        core.connect();
+        ADV.launch();
+
+
+
     }
 }
