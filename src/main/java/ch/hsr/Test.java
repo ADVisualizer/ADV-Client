0777d63fcbcade23931cbaa3f5b3d3d3afa515c2
Michael Wieland <michi.wieland@hotmail.com>
2018-03-12 13:34:22 +0100

    Updated group id and depencies

---
+++ src/main/java/ch/hsr/Test.java
@@ -0,0 +1,14 @@
+package ch.hsr;
+
+
+import ch.adv.lib.AdvCore;
+
+import java.io.IOException;
+
+public class Test {
+
+    public static final void main(String[] args) throws IOException {
+        AdvCore core = new AdvCore();
+        core.connect();
+    }
+}
