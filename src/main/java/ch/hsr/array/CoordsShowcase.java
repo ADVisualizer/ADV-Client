e9a1a65e910dcfbf75933e88be6afac8443abd46
Murièle Trentini <muriele@trentini.ch>
2018-04-22 09:46:46 +0200

    add coords showcase

---
+++ src/main/java/ch/hsr/array/CoordsShowcase.java
@@ -0,0 +1,43 @@
+package ch.hsr.array;
+
+import ch.adv.lib.core.app.ADV;
+import ch.adv.lib.core.domain.styles.*;
+import ch.adv.lib.core.domain.styles.presets.*;
+import ch.adv.lib.core.util.ADVException;
+import ch.hsr.array.model.MyArray;
+
+import java.util.Random;
+
+public class CoordsShowcase {
+    private static final int LENGTH = 5;
+
+    public static void main(String[] args) throws ADVException {
+        ADV adv = ADV.launch(args);
+        MyArray<Double> array = new MyArray<>(LENGTH, "CoordsShowcase");
+
+        Random rnd = new Random();
+        for (int i = 0; i < LENGTH; i++) {
+            array.set(i, (double)Math.round(rnd.nextDouble() * 100d) / 100d);
+
+        }
+
+        // -------- snapshot 1 -------- //
+        adv.snapshot(array, "Using no coords.");
+
+        // -------- snapshot 2 -------- //
+        array.setCoordinates(0, 100, 500);
+        array.setCoordinates(2, 500, 500);
+        adv.snapshot(array, "Using some coords");
+
+        // -------- snapshot 3 -------- //
+        array.setCoordinates(0, 100, 500);
+        array.setCoordinates(1, 300, 500);
+        array.setCoordinates(2, 500, 500);
+        array.setCoordinates(3, 700, 500);
+        array.setCoordinates(4, 900, 500);
+        adv.snapshot(array, "Using only coords");
+
+
+    }
+
+}
