cb810fcd8506a81e87575bb389c43d6e1a928ad0
Michael Wieland <michi.wieland@hotmail.com>
2018-05-23 16:27:52 +0200

    updated repository order

--- src/main/java/ch/hsr/array/StylesShowcase.java
+++ src/main/java/ch/hsr/array/StylesShowcase.java
@@ -1,7 +1,7 @@
 package ch.hsr.array;
 
 import ch.hsr.adv.commons.core.logic.domain.styles.*;
-import ch.hsr.adv.commons.core.logic.domain.styles.presets.ADVDefaultStyle;
+import ch.hsr.adv.commons.core.logic.domain.styles.presets.ADVDefaultElementStyle;
 import ch.hsr.adv.commons.core.logic.util.ADVException;
 import ch.hsr.adv.lib.array.logic.ArrayModule;
 import ch.hsr.adv.lib.bootstrapper.ADV;
@@ -26,7 +26,7 @@ public class StylesShowcase {
         }
 
         // -------- snapshot 1 -------- //
-        setStyle(0, new ADVDefaultStyle());
+        setStyle(0, new ADVDefaultElementStyle());
         setStyle(1, new ADVErrorStyle());
         setStyle(2, new ADVInfoStyle());
         setStyle(3, new ADVSuccessStyle());
