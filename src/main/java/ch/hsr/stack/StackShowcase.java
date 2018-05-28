6aee40d5d38d343d0c9b6eed89e7191099c6d1db
Michael Wieland <michi.wieland@hotmail.com>
2018-05-28 15:56:30 +0200

    updated stack showcases

--- src/main/java/ch/hsr/stack/StackShowcase.java
+++ src/main/java/ch/hsr/stack/StackShowcase.java
@@ -1,37 +1,49 @@
 package ch.hsr.stack;
 
+import ch.hsr.adv.commons.core.logic.domain.styles.ADVStyle;
 import ch.hsr.adv.commons.core.logic.util.ADVException;
 import ch.hsr.adv.lib.bootstrapper.ADV;
-import ch.hsr.adv.lib.core.logic.ADVModule;
+import ch.hsr.adv.lib.core.logic.domain.styles.presets.ADVSuccessStyle;
 import ch.hsr.adv.lib.stack.logic.StackModule;
 import ch.hsr.adv.lib.stack.logic.domain.ADVStack;
 import ch.hsr.stack.model.Stack;
 
 public class StackShowcase {
 
+    private static final ADVStack<Integer> stack = new Stack<>();
+    private static final StackModule stackModule = new StackModule("Stack", stack);
+
+
     public static void main(String[] args) throws ADVException {
         ADV.launch(args);
 
-        ADVStack<Integer> stack = new Stack<>();
-        ADVModule module = new StackModule("Stack", stack);
+        ADV.snapshot(stackModule, "Empty stack");
 
-        ADV.snapshot(module, "Empty stack");
         stack.push(1);
         stack.push(2);
-        System.out.println(stack.toString());
-        ADV.snapshot(module, "Pushed initial entries");
+        stack.push(3);
+        setStyle(0, new ADVSuccessStyle());
+
+        ADV.snapshot(stackModule, "Pushed initial entries");
+
+        stack.push(4);
+        stack.push(5);
+        ADV.snapshot(stackModule, "Pushed more entries");
 
         stack.pop();
-        System.out.println(stack.toString());
-        ADV.snapshot(module, "Poped entry");
+        ADV.snapshot(stackModule, "Popped entry");
 
         stack.push(2);
-        System.out.println(stack.toString());
-        ADV.snapshot(module, "Pushed second entry again");
+        ADV.snapshot(stackModule, "Pushed second entry again");
 
-        stack.top();
-        System.out.println(stack.toString());
-        ADV.snapshot(module, "Top does not change the stack values");
+        Integer top = stack.top();
+        System.out.println(top);
+        ADV.snapshot(stackModule, "Top does not change the stack values");
     }
+
+    private static void setStyle(int i, ADVStyle style) {
+        stackModule.getStyleMap().put(i, style);
+    }
+
 }
 
