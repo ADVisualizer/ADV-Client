625751cbd3b30f53c9b815cf968e9cd59b2b257a
Jan Winter <j4n.winter@gmail.com>
2018-11-09 11:23:37 +0100

    added examples for traversal with general trees

---
+++ src/main/java/ch/hsr/tree/model/SimpleGeneralTreeNode.java
@@ -0,0 +1,48 @@
+package ch.hsr.tree.model;
+
+import ch.hsr.adv.commons.core.logic.domain.styles.ADVStyle;
+import ch.hsr.adv.commons.tree.logic.domain.ADVGeneralTreeNode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SimpleGeneralTreeNode implements ADVGeneralTreeNode<Integer> {
+
+    private List<ADVGeneralTreeNode<Integer>> children;
+    private ADVStyle style;
+    private Integer content;
+
+    public SimpleGeneralTreeNode(Integer content) {
+        this(content, null);
+    }
+
+    private SimpleGeneralTreeNode(Integer content, ADVStyle style) {
+        children = new ArrayList<>();
+        this.content = content;
+        this.style = style;
+    }
+
+    public void addChild(SimpleGeneralTreeNode child) {
+        children.add(child);
+    }
+
+    public void setStyle(ADVStyle style) {
+        this.style = style;
+    }
+
+    @Override
+    public List<ADVGeneralTreeNode<Integer>> getChildren() {
+        return Collections.unmodifiableList(children);
+    }
+
+    @Override
+    public ADVStyle getStyle() {
+        return style;
+    }
+
+    @Override
+    public Integer getContent() {
+        return content;
+    }
+}
